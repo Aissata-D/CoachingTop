201b055865b93244a0a0bfdc50f1186715bd41ce
Aissata Diassana <diassanaaissata@yahoo.com>
2024-07-18 19:10:19 +0200

    bottom navigation relocate

---
+++ app/src/main/java/com/sitadigi/coachingtop/BottomNavigation.kt
@@ -0,0 +1,103 @@
+package com.sitadigi.coachingtop
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+public class BottomNavigationClass {
+
+    @Composable
+    public fun BottomNavigation(modifier: Modifier = Modifier) {
+        NavigationBar(
+            //   containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = modifier
+        ) {
+
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_home)
+                    )
+                },
+                selected = true,
+                onClick = {}
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_video)
+                    )
+                },
+                selected = false,
+                onClick = {}
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_planning)
+                    )
+                },
+                selected = false,
+                onClick = {}
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.ThumbUp,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_resultat)
+                    )
+                },
+                selected = false,
+                onClick = {}
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_parametre)
+                    )
+                },
+                selected = false,
+                onClick = {}
+            )
+        }
+    }
+}
--- app/src/main/java/com/sitadigi/coachingtop/MainActivity.kt
+++ app/src/main/java/com/sitadigi/coachingtop/MainActivity.kt
@@ -29,16 +29,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.res.stringResource
+import com.sitadigi.coachingtop.BottomNavigationClass
 
 class MainActivity : ComponentActivity() {
+
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val bottomNavigationClass  = BottomNavigationClass()
         setContent {
             CoachingTopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
-                    bottomBar = { BottomNavigation() },
+                    bottomBar = { bottomNavigationClass.BottomNavigation() },
                     topBar = {
                         TopAppBar(
                             colors = TopAppBarDefaults.topAppBarColors(
@@ -62,90 +66,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun BottomNavigation(modifier: Modifier = Modifier) {
-    NavigationBar(
-     //   containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        modifier = modifier
-    ) {
 
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.Home,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text(
-                    text = stringResource(R.string.bottom_navigation_home)
-                )
-            },
-            selected = true,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.PlayArrow,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text(
-                    text = stringResource(R.string.bottom_navigation_video)
-                )
-            },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.DateRange,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text(
-                    text = stringResource(R.string.bottom_navigation_planning)
-                )
-            },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.ThumbUp,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text(
-                    text = stringResource(R.string.bottom_navigation_resultat)
-                )
-            },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text(
-                    text = stringResource(R.string.bottom_navigation_parametre)
-                )
-            },
-            selected = false,
-            onClick = {}
-        )
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -166,7 +87,8 @@ fun GreetingPreview() {
 @Preview(showBackground = true)
 @Composable
 fun TestNavigation(){
+    val bottomNavigationClass  = BottomNavigationClass()
     CoachingTopTheme {
-        BottomNavigation()
+        bottomNavigationClass.BottomNavigation()
     }
 }
